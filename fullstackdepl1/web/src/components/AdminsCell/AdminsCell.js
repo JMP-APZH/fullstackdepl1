c27f14c0885455fa01111f459d63b54a780b167a
Jean-Marie Philocles <philocles.jeanmarie@gmail.com>
2023-02-03 12:38:55 +0100

    prisma schema setup - cell query for list of admins with sorted data

--- fullstackdepl1/web/src/components/AdminsCell/AdminsCell.js
+++ fullstackdepl1/web/src/components/AdminsCell/AdminsCell.js
@@ -19,10 +19,26 @@ export const Failure = ({ error }) => (
 
 export const Success = ({ adminUsers }) => {
   return (
+    // <ul>
+    //   {adminUsers.map((item) => {
+    //     return <li key={item.id}>{JSON.stringify(item)}</li>
+    //   })}
+    // </ul>
+
     <ul>
-      {adminUsers.map((item) => {
-        return <li key={item.id}>{JSON.stringify(item)}</li>
-      })}
+      {adminUsers.map((adminuser) => (
+        <adminuser key={adminuser.id} >
+        <header>
+          <h2> {adminuser.name} </h2>
+        </header>
+        <p> {adminuser.age} </p>
+
+        <img src={adminuser.avatar} />
+
+        </adminuser>
+      ))
+
+      }
     </ul>
   )
 }
